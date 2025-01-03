74978c53c406141dd70f22279e2aa89d3ee2f4a5
akak4456 <akak4456@naver.com>
2025-01-03 16:19:50 +0900

    first commit

---
+++ src/main/java/com/jo/laftelcloneserver/LaftelCloneServerApplication.java
@@ -0,0 +1,13 @@
+package com.jo.laftelcloneserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LaftelCloneServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LaftelCloneServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/jo/laftelcloneserver/LaftelCloneServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jo.laftelcloneserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LaftelCloneServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
